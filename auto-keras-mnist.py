ecf4b2423a7a2f17f86db2aa0cab742bdabe1639
Kerem <keremgocen@gmail.com>
2019-06-13 17:31:24 +0300

    use gpu

--- auto-keras-mnist.py
+++ auto-keras-mnist.py
@@ -1,6 +1,13 @@
 from keras.datasets import mnist
 from autokeras.image.image_supervised import ImageClassifier
 
+import os
+import tensorflow as tf
+os.environ['CUDA_VISIBLE_DEVICES'] = "1"
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+
+
 if __name__ == '__main__':
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train = x_train.reshape(x_train.shape + (1,))
