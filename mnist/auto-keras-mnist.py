7c8b6e02444843754b5cbf498e2ad1a28c25be03
Kerem Gocen <keremgocen@gmail.com>
2019-07-07 16:38:00 +0300

    add ubuntu gpu requirements

--- mnist/auto-keras-mnist.py
+++ mnist/auto-keras-mnist.py
@@ -18,22 +18,33 @@ Constant.SEARCH_MAX_ITER = 1
 
 
 if __name__ == '__main__':
+    # (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    # x_train = x_train.reshape(x_train.shape + (1,))
+    # x_test = x_test.reshape(x_test.shape + (1,))
+
+    # clf = ImageClassifier(path='output/', verbose=True, searcher_args={
+    #                       'trainer_args': {'max_iter_num': 1,
+    #                                        'max_no_improvement_num': 1}})
+    # clf.fit(x_train, y_train, time_limit=1 * 60 * 30)
+    # clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
+    # y = clf.evaluate(x_test, y_test)
+    # print(y)
+
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train = x_train.reshape(x_train.shape + (1,))
     x_test = x_test.reshape(x_test.shape + (1,))
 
-    clf = ImageClassifier(path='output/', verbose=True, searcher_args={
-                          'trainer_args': {'max_iter_num': 1,
-                                           'max_no_improvement_num': 1}})
-    clf.fit(x_train, y_train, time_limit=1 * 60 * 30)
+    clf = ImageClassifier(verbose=True, searcher_args={'trainer_args':{'max_iter_num':7}})
+    clf.fit(x_train, y_train, time_limit=12 * 60 * 60)
     clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
     y = clf.evaluate(x_test, y_test)
     print(y)
-    clf.export_autokeras_model('output/autokeras_model')
+
+    clf.export_autokeras_model('output/auto_mnist_model')
 
     # alternative
     best_model = clf.cnn.best_model.produce_model()
-    # pickle_to_file(best_model, 'output/autokeras_best_model')
+    pickle_to_file(best_model, 'output/auto_mnist_best_model')
     print(best_model)
 
 # Step 2 : After the model training is complete, run examples/visualize.py, whilst passing the same path as parameter
